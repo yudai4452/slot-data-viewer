c4754744be3b3ce2798e678ad297b077bfcc335d
yudai4452 <yudaiprogram@gmail.com>
2025-03-27 00:24:23 +0900

    Update slot_data_viewer_gdrive.py

--- slot_data_viewer_gdrive.py
+++ slot_data_viewer_gdrive.py
@@ -70,7 +70,9 @@ try:
         # plt.get_cmapで10段階の離散カラーマップを作成
         #c = ax2.imshow(pivot_df, aspect="auto", cmap=plt.get_cmap("viridis", 10), interpolation='none', vmin=vmin, vmax=vmax)
         # plasmaカラーマップ（10段階）
-        c = ax2.imshow(pivot_df, aspect="auto", cmap=plt.get_cmap("plasma", 10), interpolation='none', vmin=vmin, vmax=vmax)
+        #c = ax2.imshow(pivot_df, aspect="auto", cmap=plt.get_cmap("plasma", 10), interpolation='none', vmin=vmin, vmax=vmax)
+        # cividisカラーマップ（10段階）
+        c = ax2.imshow(pivot_df, aspect="auto", cmap=plt.get_cmap("cividis", 10), interpolation='none', vmin=vmin, vmax=vmax)
         
         ax2.set_title(f"{model} の {heatmap_col} ヒートマップ（{store}）")
         ax2.set_xlabel("日付")
