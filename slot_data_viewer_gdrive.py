361297388bb2a3f7b379de6f37de10e6208d2446
yudai4452 <yudaiprogram@gmail.com>
2025-03-27 00:15:46 +0900

    Update slot_data_viewer_gdrive.py

--- slot_data_viewer_gdrive.py
+++ slot_data_viewer_gdrive.py
@@ -67,7 +67,8 @@ try:
         # vmin, vmaxで色のスケールを固定
         vmin = pivot_df.min().min()
         vmax = pivot_df.max().max()
-        c = ax2.imshow(pivot_df, aspect="auto", cmap="viridis", interpolation='none', vmin=vmin, vmax=vmax)
+        # plt.get_cmapで10段階の離散カラーマップを作成
+        c = ax2.imshow(pivot_df, aspect="auto", cmap=plt.get_cmap("viridis", 10), interpolation='none', vmin=vmin, vmax=vmax)
         
         ax2.set_title(f"{model} の {heatmap_col} ヒートマップ（{store}）")
         ax2.set_xlabel("日付")
